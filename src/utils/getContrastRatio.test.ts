6d575f8a32f56ef7045486f56dd1b3af37719b7e
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-10 16:31:01 +1000

    Refactor tests to use precise variables and wording

--- src/utils/getContrastRatio.test.ts
+++ src/utils/getContrastRatio.test.ts
@@ -1,10 +1,10 @@
 import {getContrastRatio} from "./getContrastRatio"
 
 describe("getContrastRatio", () => {
-    it("happy ending", () => {
+    it("happy path", () => {
         const l1 = 0
         const l2 = 1
         const output = 21
-        expect(getContrastRatio(l1,l2)).toBe(21)
+        expect(getContrastRatio(l1,l2)).toBe(output)
     })
 })
--- src/utils/getLuminanceValue.test.ts
+++ src/utils/getLuminanceValue.test.ts
@@ -1,7 +1,7 @@
 import {getLuminanceValue} from "./getLuminanceValue"
 
 describe("getLuminanceValue", () => {
-    it("happy ending", () => {
+    it("happy path", () => {
         const input = "rgb(255,128,0)"
         const output = 0.36700000000000004
         expect(getLuminanceValue(input)).toBeCloseTo(output, 3)
