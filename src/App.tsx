d2a65ee12a0c7382056aff4b85b971ff1d00c350
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-10 16:52:29 +1000

    - Remove handleBlur and onBlur here - i dont need it anymore - calculate contrast ratio - Render contrast ratio - put a guard on contrast ratio UI when user in mid keystroke

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,51 @@
 import { useState } from 'react';
-import './App.css';
+import { getLuminanceValue } from "./utils/getLuminanceValue"
+import { getContrastRatio } from "./utils/getContrastRatio"
 
 function App() {
-  const [backgroundRGB, setBackgroundRGB] = useState<string>('');
-  const [foregroundRGB, setForegroundRGB] = useState('');
+  const [backgroundRGB, setBackgroundRGB] = useState<string>('rgb(255,255,255)');
+  const [foregroundRGB, setForegroundRGB] = useState('rgb(0,0,0)');
 
-  const handleBlur = () => {
-    setBackgroundRGB('');
-    setForegroundRGB('');
-  };
+  const backgroundLuminanceValue = getLuminanceValue(backgroundRGB)
+  const foregroundLuminanceValue = getLuminanceValue(foregroundRGB)
+  const contrastRatio = getContrastRatio(backgroundLuminanceValue, foregroundLuminanceValue)
+
 
   return (
     <>
+    <div>
+      <div>
       <label>
         Background RGB:{' '}
         <input
-          onBlur={handleBlur}
           onChange={(e) => setBackgroundRGB(e.target.value)}
           placeholder="rgb(255,255,255)"
           type="text"
           value={backgroundRGB}
         />
       </label>
+      </div>
+      <div>
       <label>
         Foreground RGB:{' '}
         <input
-          onBlur={handleBlur}
           onChange={(e) => setForegroundRGB(e.target.value)}
           placeholder="rgb(0,0,0)"
           type="text"
           value={foregroundRGB}
         />
       </label>
+      </div>
       <small>RGB Color Example: rgb(255,255,255)</small>
+    </div>
+
+    <hr />
+    
+    <div>
+      <p>Contrast Ratio: {(isNaN(backgroundLuminanceValue) || isNaN(foregroundLuminanceValue)) ? "Enter valid RGB values 🐖" : contrastRatio}</p>
+    </div>
+    
+    <hr />
     </>
   );
 }
