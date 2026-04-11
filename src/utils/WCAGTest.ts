619e9cfdc57822d81bde08621d99bb2be2835164
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-11 15:53:31 +1000

    Refactor the WCAGTest to return an array of object with the data i want in order to do preview text live on WCAG table.

--- src/utils/WCAGTest.ts
+++ src/utils/WCAGTest.ts
@@ -1,15 +1,42 @@
-type WCAGResult = {
+type WCAGResultA = {
   NormalAA: "✅ Pass" | "😭 Fail";
   NormalAAA: "✅ Pass" | "😭 Fail";
   LargeAA: "✅ Pass" | "😭 Fail";
   LargeAAA: "✅ Pass" | "😭 Fail";
 };
 
-export function WCAGTest(contrastRatio: number): WCAGResult {
+export function WCAGTestA(contrastRatio: number): WCAGResultA {
   return {
     NormalAA: contrastRatio >= 4.5 ? "✅ Pass" : "😭 Fail",
     NormalAAA: contrastRatio >= 7 ? "✅ Pass" : "😭 Fail",
     LargeAA: contrastRatio >= 3 ? "✅ Pass" : "😭 Fail",
     LargeAAA: contrastRatio >= 4.5 ? "✅ Pass" : "😭 Fail",
   };
+}
+
+type WCAGRowResult = {
+  level: string;
+  minContrast: number;
+  passes: boolean;
+};
+
+export function WCAGTest(contrastRatio: number): WCAGRowResult[] {
+  return [
+    {
+      level: "AA for normal text",
+      minContrast: 4.5,
+      passes: contrastRatio >= 4.5,
+    },
+    {
+      level: "AAA for normal text",
+      minContrast: 7,
+      passes: contrastRatio >= 7,
+    },
+    { level: "AA for large text", minContrast: 3, passes: contrastRatio >= 3 },
+    {
+      level: "AAA for large text",
+      minContrast: 4.5,
+      passes: contrastRatio >= 4.5,
+    },
+  ];
 }
